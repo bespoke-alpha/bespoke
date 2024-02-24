5c0ca0d186b938d4da640d8347bf03c1e6cdc029
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-02-24 23:20:26 +0100

    y

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,16 +1,15 @@
-import { modules } from "./module.js"
-import { applyTransforms } from "./transforms/transforms.js"
-import "./transforms/devtools.js"
-import "./transforms/styledComponents.js"
-import "./transforms/themes.js"
+import { modules } from "./module.js";
+import { applyTransforms } from "./transforms/transforms.js";
+import "./transforms/devtools.js";
+import "./transforms/styledComponents.js";
 
-await modules.reduce((p, module) => p.then(() => module.loadMixin()), Promise.resolve())
-await Promise.all(["/vendor~xpui.js", "/xpui.js"].map(applyTransforms).map(async p => import(await p)))
+await modules.reduce((p, module) => p.then(() => module.loadMixin()), Promise.resolve());
+await Promise.all(["/vendor~xpui.js", "/xpui.js"].map(applyTransforms).map(async p => import(await p)));
 
-import { internalModule } from "./module.js"
-const { awaitedMixins } = internalModule
-console.info(awaitedMixins)
-await Promise.all(awaitedMixins)
+import { internalModule } from "./module.js";
+const { awaitedMixins } = internalModule;
+console.info(awaitedMixins);
+await Promise.all(awaitedMixins);
 
-for (const module of modules) module.loadCSS()
-modules.reduce((p, module) => p.then(() => module.loadJS()), Promise.resolve())
+for (const module of modules) module.loadCSS();
+modules.reduce((p, module) => p.then(() => module.loadJS()), Promise.resolve());
