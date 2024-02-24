4a8d85bfc4332e63be592363bbd2a79d0afc3f19
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-02-24 05:14:53 +0100

    y

--- modules/library/db.js
+++ modules/library/db.js
@@ -17,6 +17,36 @@ export const db = new (class extends Dexie {
         });
     }
 })();
+// export const getMainUrisForIsrcs = async (isrcs: string[]) => {
+//     const tracks = await db.isrcs.bulkGet(isrcs)
+//     const missedTracks = tracks.reduce((missed, track, i) => {
+//         track || missed.push(i)
+//         return missed
+//     }, [] as number[])
+//     if (missedTracks.length) {
+//         const missedIsrcs = missedTracks.map(i => isrcs[i])
+//         const resultsIsrcs = await Promise.allSettled(missedIsrcs.map(isrc => searchTracks(`isrc:${isrc}`, 0, 1)))
+//         const filledTracks = _.compact(
+//             resultsIsrcs.map((resultsIsrc, i) => {
+//                 const isrc = isrcs[i]
+//                 if (resultsIsrc.status === "fulfilled") {
+//                     const uri = resultsIsrc.value[0]?.item.data.uri
+//                     if (!uri) {
+//                         console.error("Couldn't get matching track for isrc:", isrc)
+//                         return
+//                     }
+//                     return { isrc, uri }
+//                 }
+//                 console.error("Failed searching track for isrc:", isrc)
+//             }),
+//         )
+//         db.isrcs.bulkAdd(filledTracks)
+//         missedTracks.forEach((missedTrack, i) => {
+//             tracks[missedTrack] = filledTracks[i]
+//         })
+//     }
+//     return tracks.map(track => track?.uri)
+// }
 export const getISRCsForUris = async (uris) => {
     const tracks = await db.tracks.bulkGet(uris);
     const missedTracks = tracks.reduce((missed, track, i) => {
--- modules/library/index.js
+++ modules/library/index.js
@@ -2,36 +2,6 @@ import { _ } from "/hooks/deps.js";
 import { onTrackListMutationListeners } from "../delulib/listeners.js";
 import { db, getISRCsForUris } from "./db.js";
 import { PlaylistItems } from "./listeners.js";
-// export const getMainUrisForIsrcs = async (isrcs: string[]) => {
-//     const tracks = await db.isrcs.bulkGet(isrcs)
-//     const missedTracks = tracks.reduce((missed, track, i) => {
-//         track || missed.push(i)
-//         return missed
-//     }, [] as number[])
-//     if (missedTracks.length) {
-//         const missedIsrcs = missedTracks.map(i => isrcs[i])
-//         const resultsIsrcs = await Promise.allSettled(missedIsrcs.map(isrc => searchTracks(`isrc:${isrc}`, 0, 1)))
-//         const filledTracks = _.compact(
-//             resultsIsrcs.map((resultsIsrc, i) => {
-//                 const isrc = isrcs[i]
-//                 if (resultsIsrc.status === "fulfilled") {
-//                     const uri = resultsIsrc.value[0]?.item.data.uri
-//                     if (!uri) {
-//                         console.error("Couldn't get matching track for isrc:", isrc)
-//                         return
-//                     }
-//                     return { isrc, uri }
-//                 }
-//                 console.error("Failed searching track for isrc:", isrc)
-//             }),
-//         )
-//         db.isrcs.bulkAdd(filledTracks)
-//         missedTracks.forEach((missedTrack, i) => {
-//             tracks[missedTrack] = filledTracks[i]
-//         })
-//     }
-//     return tracks.map(track => track?.uri)
-// }
 const setTrackGreyed = (track, greyed) => {
     track.style.backgroundColor = greyed ? "gray" : undefined;
     track.style.opacity = greyed ? "0.3" : "1";
