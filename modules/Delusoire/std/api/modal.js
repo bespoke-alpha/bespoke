8d76cc5d2d2b9dbe38da9f0c7460986d91dda03c
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-02-29 22:17:02 +0100

    y

--- modules/Delusoire/std/api/modal.js
+++ modules/Delusoire/std/api/modal.js
@@ -21,7 +21,16 @@ export function display({ title: contentLabel, content: children, isLarge: isEmb
                         }))),
                     S.React.createElement("div", { className: "IJHNf0vxPSbPE1egoG4N" }, children))));
         }
-        return; // TODO: implement TrackCreditsModal
+        return (S.React.createElement(GenericModal, { isOpen: isOpen, contentLabel: contentLabel },
+            S.React.createElement("div", { className: "uV8q95GGAb2VDtL3gpYa" },
+                S.React.createElement("div", { className: "pGU_qEtNT1qWKjrRbvan" },
+                    S.React.createElement(Text, { as: "h1", variant: "titleMedium" }, contentLabel),
+                    S.React.createElement("button", { className: "VKCcyYujazVPj6VkksPM", "aria-label": Locale.get("close"), onClick: close }, createIconComponent({
+                        icon: "<path d='M2.47 2.47a.75.75 0 0 1 1.06 0L8 6.94l4.47-4.47a.75.75 0 1 1 1.06 1.06L9.06 8l4.47 4.47a.75.75 0 1 1-1.06 1.06L8 9.06l-4.47 4.47a.75.75 0 0 1-1.06-1.06L6.94 8 2.47 3.53a.75.75 0 0 1 0-1.06Z'/>",
+                        "aria-label": Locale.get("close"),
+                        iconSize: 18,
+                    }))),
+                S.React.createElement("div", { className: "Nw1INlIyra3LT1JjvoqH" }, children))));
     };
     ref = S.React.createElement(PopupModal);
     RootRegistry.register(ref, () => true);
--- modules/Delusoire/std/api/modal.tsx
+++ modules/Delusoire/std/api/modal.tsx
@@ -46,7 +46,25 @@ export function display({
 			);
 		}
 
-		return; // TODO: implement TrackCreditsModal
+		return (
+			<GenericModal isOpen={isOpen} contentLabel={contentLabel}>
+				<div className="uV8q95GGAb2VDtL3gpYa">
+					<div className="pGU_qEtNT1qWKjrRbvan">
+						<Text as="h1" variant="titleMedium">
+							{contentLabel}
+						</Text>
+						<button className="VKCcyYujazVPj6VkksPM" aria-label={Locale.get("close")} onClick={close}>
+							{createIconComponent({
+								icon: "<path d='M2.47 2.47a.75.75 0 0 1 1.06 0L8 6.94l4.47-4.47a.75.75 0 1 1 1.06 1.06L9.06 8l4.47 4.47a.75.75 0 1 1-1.06 1.06L8 9.06l-4.47 4.47a.75.75 0 0 1-1.06-1.06L6.94 8 2.47 3.53a.75.75 0 0 1 0-1.06Z'/>",
+								"aria-label": Locale.get("close"),
+								iconSize: 18,
+							})}
+						</button>
+					</div>
+					<div className="Nw1INlIyra3LT1JjvoqH">{children}</div>
+				</div>
+			</GenericModal>
+		);
 	};
 	ref = S.React.createElement(PopupModal);
 	RootRegistry.register(ref, () => true);
