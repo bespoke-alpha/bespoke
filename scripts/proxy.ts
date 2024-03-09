c7dca70e86afd5f723ad422b47cbfdc7c6d7bf0d
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-10 00:44:25 +0100

    y

--- scripts/proxy.ts
+++ scripts/proxy.ts
@@ -1,41 +1,39 @@
-import { Elysia } from "elysia"
-import { cors } from "@elysiajs/cors"
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const xSetUrl = "X-Set-Url"
-const xSetHeaders = "X-Set-Headers"
+const xSetUrl = "X-Set-Url";
+const xSetHeaders = "X-Set-Headers";
 
 new Elysia()
-    .use(cors())
-    .get("/", async context => {
-        // return new Response(undefined, { status: 418 })
-        try {
-            const req = context.request
-            const url = req.headers.get(xSetUrl)!
-            const xForbiddenHeaders = JSON.parse(req.headers.get(xSetHeaders))
-            req.headers.delete(xSetUrl)
-            req.headers.delete(xSetHeaders)
+	.use(cors())
+	.get("/", async context => {
+		// return new Response(undefined, { status: 418 })
+		try {
+			const req = context.request;
+			const url = req.headers.get(xSetUrl)!;
+			const headers = JSON.parse(req.headers.get(xSetHeaders));
+			req.headers.delete(xSetUrl);
+			req.headers.delete(xSetHeaders);
 
-            req.headers.delete("host")
-            if (xForbiddenHeaders) {
-                for (const [k, v] of Object.entries(xForbiddenHeaders)) {
-                    if (v === null) {
-                        req.headers.delete(k)
-                    } else {
-                        req.headers.set(k, v)
-                    }
-                }
-            }
+			req.headers.delete("host");
+			for (const [k, v] of Object.entries(headers ?? {})) {
+				if (v === null) {
+					req.headers.delete(k);
+				} else {
+					req.headers.set(k, v);
+				}
+			}
 
-            const res = await fetch(url, req)
+			const res = await fetch(url, req);
 
-            for (const h of ["Access-Control-Allow-Origin", "Content-Encoding", "Date"]) {
-                res.headers.delete(h)
-            }
+			for (const k of ["Access-Control-Allow-Origin", "Content-Encoding", "Date"]) {
+				res.headers.delete(k);
+			}
 
-            return res
-        } catch (e) {
-            console.error(e)
-            throw e
-        }
-    })
-    .listen(3000)
+			return res;
+		} catch (e) {
+			console.error(e);
+			throw e;
+		}
+	})
+	.listen(3000);
