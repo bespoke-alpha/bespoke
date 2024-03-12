7381f45e7dd13884f7e30480d5a8a11fc8581901
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-12 14:39:36 +0000

    y

--- hooks-out/types/transforms/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const internalRegisterTransform: <A = void>({ transform, then, glob, noAwait }: import("./transform.js").MixinProps<A>) => void;
-export declare const applyTransforms: (path: string) => string | Promise<string>;
---
+++ modules/tailwind.config.ts
@@ -0,0 +1,1 @@
+// this file is a requirement for the tailwindcss vscode extension to activate
