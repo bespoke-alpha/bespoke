86d40e13ad218534f713c5d4fea524a3a76727fb
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-25 03:01:59 +0000

    update proxy

--- proxy/index.ts
+++ proxy/index.ts
@@ -15,57 +15,14 @@ export default new Elysia({ aot: false })
 	)
 	.all("/mitm/*", async context => {
 		// return new Response(undefined, { status: 418 })
-		let resClone: Response;
+		let res: Response;
 
 		const logs = [];
 		let err: any;
 		try {
 			const req = new Request(context.request, { body: context.body });
-			const reqUrlObj = new URL(context.request.url);
 			const urlPath = context.params["*"];
-			const urlSearch = reqUrlObj.search;
-			const url = urlPath + urlSearch;
-			const headers = JSON.parse(req.headers.get(xSetHeaders));
-			req.headers.delete(xSetHeaders);
-
-			req.headers.delete("host");
-			for (const [k, v] of Object.entries(headers ?? {})) {
-				if (v === "undefined") {
-					req.headers.delete(k);
-				} else {
-					req.headers.set(k, v);
-				}
-			}
-
-			logs.push(new Date());
-			logs.push(url);
-			logs.push(...Array.from(req.headers.entries()).map(([k, v]) => `${k}: ${v}`));
-			const res = await fetch(url, req);
-			resClone = new Response(res.body, res);
-			logs.push(await resClone.clone().text());
-
-			for (const k of ["Access-Control-Allow-Origin", "Content-Encoding", "Date"]) {
-				resClone.headers.delete(k);
-			}
-
-			if (resClone.headers.has("Location")) {
-				const locationHeader = resClone.headers.get("Location");
-
-				let locationUrlObj: URL;
-				try {
-					locationUrlObj = new URL(locationHeader);
-					locationUrlObj = new URL(`${reqUrlObj.origin}/mitm/${locationUrlObj.href}`);
-				} catch (_) {
-					locationUrlObj = new URL(reqUrlObj);
-					locationUrlObj.search = "";
-					if (!locationUrlObj.href.endsWith("/")) {
-						locationUrlObj.href += "/";
-					}
-					locationUrlObj.href += locationHeader;
-				}
-
-				resClone.headers.set("Location", locationUrlObj.href);
-			}
+			res = await handleMitm(req, urlPath, logs);
 		} catch (e) {
 			err = e;
 		}
@@ -77,7 +34,8 @@ export default new Elysia({ aot: false })
 			console.error(err);
 			throw err;
 		}
-		return resClone;
+
+		return res;
 	})
 	.get("/ping/", () => new Response("pong", { status: 200 })) // TODO: can be used to track launches
 	.get("/protocol/*", async context => {
@@ -98,3 +56,51 @@ export default new Elysia({ aot: false })
 		return new Blob([html], { type: "text/html" });
 	})
 	.listen(8787);
+
+const handleMitm = async (req: Request, urlPath: string, logs: any[]) => {
+	const reqUrlObj = new URL(req.url);
+	const urlSearch = reqUrlObj.search;
+	const url = urlPath + urlSearch;
+	const headers = JSON.parse(req.headers.get(xSetHeaders));
+	req.headers.delete(xSetHeaders);
+
+	req.headers.delete("host");
+	for (const [k, v] of Object.entries(headers ?? {})) {
+		if (v === "undefined") {
+			req.headers.delete(k);
+		} else {
+			req.headers.set(k, v);
+		}
+	}
+
+	logs.push(new Date());
+	logs.push(url);
+	logs.push(...Array.from(req.headers.entries()).map(([k, v]) => `${k}: ${v}`));
+	const res = await fetch(url, req);
+	const resClone = new Response(res.body, res);
+	logs.push(await resClone.clone().text());
+
+	for (const k of ["Access-Control-Allow-Origin", "Content-Encoding", "Date"]) {
+		resClone.headers.delete(k);
+	}
+
+	if (resClone.headers.has("Location")) {
+		const locationHeader = resClone.headers.get("Location");
+
+		let locationUrlObj: URL;
+		try {
+			locationUrlObj = new URL(locationHeader);
+			locationUrlObj = new URL(`${reqUrlObj.origin}/mitm/${locationUrlObj.href}`);
+		} catch (_) {
+			locationUrlObj = new URL(reqUrlObj);
+			locationUrlObj.search = "";
+			if (!locationUrlObj.href.endsWith("/")) {
+				locationUrlObj.href += "/";
+			}
+			locationUrlObj.href += locationHeader;
+		}
+
+		resClone.headers.set("Location", locationUrlObj.href);
+	}
+	return resClone;
+};
